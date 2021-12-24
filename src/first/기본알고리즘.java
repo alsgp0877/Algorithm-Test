76b3cd32188fd42752e4901209c37e29f1a0fbe6
alsgp0877 <alsgp0877@naver.com>
2021-12-25 00:09:53 +0900

    도메인주소에 '.' 이외의 문자가 있다면 이메일 조건에 안맞으므로 로컬 이름 검사하기전에 false

--- src/first/기본알고리즘.java
+++ src/first/기본알고리즘.java
@@ -7,35 +7,34 @@ public class 기본알고리즘 {
 	public static void main(String[] args) {
 
 		String mail = "alsgp0877@naver.com";
-		boolean firstTextMail = mail.startsWith("+");
 		boolean chkLowerCase = false;
 
 		if (mail != null) {
 			if (1 <= mail.length() && mail.length() <= 100) {
-				if (!firstTextMail) {
-					if (mail.contains("@")) {
-						
-						String[] mailSplit = mail.split("@");
-						String f = mailSplit[0];
-						String b = mailSplit[1];
-						
-						if(f.contains("+")) {
-							//f문자열 1~3까지의 문자열
-							//+기호가 시작되는 부분부터 끝까지 문자열
-							//그 문자열을 f문자열에서 찾아서 빈칸으로 만들기
-							String rsubstring = f.substring(f.indexOf("+",0), f.length());
+				if (mail.contains("@")) {
+
+					String[] mailSplit = mail.split("@");
+					String f = mailSplit[0];
+					String b = mailSplit[1];
+
+					boolean firstTextMail = f.startsWith("+");
+					if (b.matches()) {// TODO .이외의 문자가 들어있다면 false 밑에 연산을 더이상수행하지않음
+						if (f.contains("+")) {
+							if (!firstTextMail) {
+								// f문자열 1~3까지의 문자열
+								// +기호가 시작되는 부분부터 끝까지 문자열
+								// 그 문자열을 f문자열에서 찾아서 빈칸으로 만들기
+								String rsubstring = f.substring(f.indexOf("+", 0), f.length());
+								f.replace(rsubstring, "");
+							}
+						}
+
+						if (f.contains(".")) {
+
+							// f문자열에서 .있는 위치를 반환해서 그 위치 문자열을 알아낸후 그만큼한 빈칸으로 변경
+							String rsubstring = f.substring(f.indexOf(".", 0));
 							f.replace(rsubstring, "");
 						}
-						
-						if(f.contains(".")) {
-							
-							//f문자열에서 .있는 위치를 반환해서 그 위치 문자열을 알아낸후 그만큼한 빈칸으로 변경
-							String rsubstring = f.substring(f.indexOf(".",0));
-							f.replace(rsubstring, "");
-						}
-						
-						//TODO 도메인은 .문자만허용 + 문자는 허용안함
-						
 					}
 				}
 			}
