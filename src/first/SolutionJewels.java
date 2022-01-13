0d97b078011a23513a291b913651e2338f7ef627
alsgp0877 <alsgp0877@naver.com>
2022-01-14 00:52:12 +0900

    보석과돌 aA가 보석으로 한번 쓰였으면 그다음은 aA는 사용안되고 B처럼 다른거 사용해야함

---
+++ src/first/SolutionJewels.java
@@ -0,0 +1,16 @@
+package first;
+
+public class SolutionJewels {
+
+	public static void main(String[] args) {
+	
+		
+	}
+	
+	private static int solve(String jew,String stones) {
+		return 0;
+		
+	}
+
+	
+}
