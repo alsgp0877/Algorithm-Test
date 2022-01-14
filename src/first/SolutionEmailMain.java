89231bcce57dc0e70a1e6a95b11ff3ba6cb8b08f
alsgp0877 <alsgp0877@naver.com>
2022-01-15 01:43:01 +0900

    고유한이메일 문제 main class 다시 추가

---
+++ src/first/SolutionEmailMain.java
@@ -0,0 +1,14 @@
+package first;
+
+public class SolutionEmailMain {
+	
+	public static void main(String[] args) {
+		
+		String[] mailCollection = {"test.email+james@coding.com"};
+		SolutionEmail slove = new SolutionEmail();
+		System.out.println(slove.solve(mailCollection));
+		
+	}
+	
+}
+
