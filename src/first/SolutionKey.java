b48fb2045341c211d1ae9c1b641519497d3cf662
alsgp0877 <alsgp0877@naver.com>
2022-01-19 23:59:18 +0900

    문제3번 검색해서 알아서 그런지 생각보다 빨리풀었음.. 뒤에서..앞으로..

--- src/first/SolutionKey.java
+++ src/first/SolutionKey.java
@@ -5,8 +5,9 @@ public class SolutionKey {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String key = "8F3Z-2e-9-w";
-		solve(key,4);
+		/* String key = "8F3Z-2e-9-w"; */
+		String key = "8-5g-3-J";
+		System.out.println("재 포맷된 라이센스키:" + solve(key,2));
 
 	}
 	
@@ -18,10 +19,10 @@ public class SolutionKey {
 		
 		//문자열이 들어왔을때 첫대쉬를 기준으로 앞뒤를 나눈다. 
 		String[] str = s.split("-");
-		System.out.println(str[0]);//첫문자
-		System.out.println(str[1]);//나
-		System.out.println(str[2]);//머
-		System.out.println(str[3]);//지 
+		//System.out.println(str[0]);//첫문자
+		//System.out.println(str[1]);//나
+		//System.out.println(str[2]);//머
+		//System.out.println(str[3]);//지 
 		StringBuilder second = new StringBuilder();
 		String first="";
 		
@@ -38,16 +39,26 @@ public class SolutionKey {
 		//뒷부분에서 이제 k로 들어온 값을 가지고 나눠야하는데 
 		//만약에 k가 2가 들어왔으면 2-2 이란식으로 해줘야함. 
 		
-		System.out.println(first.toUpperCase() + '-' + second.toString().toUpperCase());//지 
-
-		for(int i=0;i<second.length();i++) {
+		//System.out.println(first.toUpperCase() + '-' + second.toString().toUpperCase());//지 
+		
+		System.out.println("길이" + second.length());
+		
+		//뒤에서부터 와야 정확한 문자의 길이를 알고잘라낼수있어서 그런듯?
+		//-1한거는 맨뒤에는 붙으면 안되서 
+		for(int i=second.length()-1;i>0;i--) {
 			//https://okky.kr/article/230197
-			
+			if(i%k==0) {//4번째 자리에 - 넣기, 4로나눴을때 나머지가 0인곳이 4의 배수나 그런곳의 자리이겟지..
+				second.insert(i, "-");
+			}
 			
 		}
 		
+		System.out.println("기호나누기" + second);
 		
-		return null;
+		String result = first +"-" + second;
+		
+		
+		return result;
 		
 	}
 
