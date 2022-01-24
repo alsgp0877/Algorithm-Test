e9216c5b995ccc66941fd1c4f2691f1359652cc6
alsgp0877 <alsgp0877@naver.com>
2022-01-24 23:58:45 +0900

    9에다가 1더했을때 앞에 앞에 1 주고 현재자릿수는 0이 되도록 하긴했는데..이거 계속 -1..-2..해야하나염..

--- src/first/SolutionPlus.java
+++ src/first/SolutionPlus.java
@@ -14,15 +14,19 @@ public class SolutionPlus {
 	
 	public static int solve(int[] k) {
 
-		//1 배열의 맨끝에 있는 숫자
 		int a = k[k.length-1];
-		System.out.println("맨뒤에숫자"+a);
-		//배열이 맨끝에 있는 숫자 +1
-		int b = a+1;
-		System.out.println("맨뒤에숫자"+b);
-		//배열의 맨끝에 있는 숫자를 +1 된 숫자로 바꿈
-		k[k.length-1] = b;
-		System.out.println("배열"+ Arrays.toString(k));
+
+		if(a == 9) {
+			k[k.length-1] = 0;
+			int c = k[k.length-2]+1;
+			k[k.length-2] = c;
+			System.out.println("9배열"+ Arrays.toString(k));
+			
+		}else {
+			int b = a+1;
+			k[k.length-1] = b;
+			System.out.println("그냥숫자배열"+ Arrays.toString(k));
+		}
 		
 		return 1;
 		
