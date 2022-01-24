27ca0d58a69bdf9616d62339b531e36c48994af8
alsgp0877 <alsgp0877@naver.com>
2022-01-25 00:34:53 +0900

    9,9일때 맨앞에 있는값 어떻게 2자리로 늘려서 배열에 넣는지 생각하기

--- src/first/SolutionPlus.java
+++ src/first/SolutionPlus.java
@@ -7,26 +7,37 @@ public class SolutionPlus {
 
 	public static void main(String[] args) {
 		
-		int[] digits = {1,2,9};
+		//int[] digits = {1,2,9};//9가 1의 자리인경우 
+		int[] digits = {9,9};//9가 1의 자리10의자리 인경우 
 		solve(digits);
 
 	}
 	
 	public static int solve(int[] k) {
-
-		int a = k[k.length-1];
-
-		if(a == 9) {
-			k[k.length-1] = 0;
-			int c = k[k.length-2]+1;
-			k[k.length-2] = c;
-			System.out.println("9배열"+ Arrays.toString(k));
+		
+		for(int i=k.length-1;i>0;i--) {//끝에서부터 
 			
-		}else {
-			int b = a+1;
-			k[k.length-1] = b;
-			System.out.println("그냥숫자배열"+ Arrays.toString(k));
+			int a = k[i];
+			
+			if(a >= 9) {
+				k[i] = 0;
+				int c = k[i-1]+1;
+				if(k[0] >= 9) {
+					//1,0
+					k[i-1] = 0;
+					k[i-2] = 1;
+				}else {
+					k[i-1] = c;
+				}
+			}else {
+				int b = a+1;
+				k[i] = b;
+				break;
+			}
 		}
+		
+		System.out.println("배열"+ Arrays.toString(k));
+
 		
 		return 1;
 		
