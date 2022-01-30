39d74e38de5bef44b96acad210895283e7a698ce
alsgp0877 <alsgp0877@naver.com>
2022-01-31 01:11:35 +0900

    도저히 생각안나서 답봤는데 거의 비슷하게 생각하긴 했는데 중간에 return 해서 값 내줄생각을 못했네..무조건 마지막에 답을 줘야한다고 생각함 ㅠㅠ

--- src/first/SolutionPlus.java
+++ src/first/SolutionPlus.java
@@ -7,40 +7,27 @@ public class SolutionPlus {
 
 	public static void main(String[] args) {
 		
-		//int[] digits = {1,2,9};//9가 1의 자리인경우 
-		int[] digits = {9,9};//9가 1의 자리10의자리 인경우 
-		solve(digits);
+		int[] digits = {2,2,9};//9가 1의 자리인경우 
+		//int[] digits = {9,9};//9가 1의 자리10의자리 인경우 
+		System.out.println(Arrays.toString(solve(digits)));
 
 	}
 	
-	public static int solve(int[] k) {
+	public static int[] solve(int[] k) {
 		
-		for(int i=k.length-1;i>0;i--) {//끝에서부터 
-			
-			int a = k[i];
-			
-			if(a >= 9) {
-				k[i] = 0;
-				int c = k[i-1]+1;
-				if(k[0] >= 9) {
-					//1,0
-					k[i-1] = 0;
-					k[i-2] = 1;
-				}else {
-					k[i-1] = c;
-				}
-			}else {
-				int b = a+1;
-				k[i] = b;
-				break;
+		int n = k.length;//
+		
+		for(int i=n-1;i>=0;i--) {
+			k[i]++;
+			if(k[i]<10) {
+				return k;//999가 아닐때는 중간에 return
 			}
+			k[i]=0;
+			
 		}
-		
-		System.out.println("배열"+ Arrays.toString(k));
-
-		
-		return 1;
-		
+		int[] result = new int[n+1];//0,0,0,0 => 이거 원래 0으로 기본값이 나와서 그걸로 사용하는거임..
+		result[0]=1;
+		return result;
 	}
 
 }
